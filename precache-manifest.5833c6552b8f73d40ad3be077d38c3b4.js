bbf77d49be30904a02e710d3768fd776c4ef4c3b
Akanksha <akankshagehlot1220@gmail.com>
2020-07-22 01:13:08 +0530

    Updates

---
+++ precache-manifest.5833c6552b8f73d40ad3be077d38c3b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "279800a228ee76f8625f57de37bc57d1",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "95a3dc017be8ce5c8fcd",
+    "url": "/todo-list/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "534de44c945844825651",
+    "url": "/todo-list/static/js/2.c06bdd37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.c06bdd37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a3dc017be8ce5c8fcd",
+    "url": "/todo-list/static/js/main.dfd43158.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
